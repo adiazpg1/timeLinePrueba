59085fc5287e888d7ba72b0a84446ad649999577
Ariel Diaz <adiaz@perceptiongrp.com>
2018-03-09 17:48:40 -0300

    commit fin de semana

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Timeline } from '../timeline/timeline';
+import { SharepointService } from '../../services/sharepoint.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  itemSelected: Timeline;
+  totalItems: number;
+  data: Timeline[] = [];
+
+  constructor(private _sharepointService: SharepointService) {
+
+    // this._sharepointService.getItems().subscribe(
+    //   (result: any) => {
+    //     this.data = result.d.results;
+    //     console.log('This.data :' + this.data);
+
+    //     for (let i = 0; i < this.data.length; i++) {
+    //       // tslint:disable-next-line:prefer-const
+    //       let item: TimelineElement = {
+    //         caption: '',
+    //         title: this.data[i].Title,
+    //         content: this.data[i].Descripcion,
+    //         visible: this.data[i].Visible,
+    //         date: new Date(this.data[i].Fecha),
+    //         link: this.data[i].Link.Url
+    //       };
+    //       this.listaNoticias.push(item);
+    //     }
+    //     console.log(this.listaNoticias);
+    //   },
+    //   error => {
+    //     console.log(<any>error);
+    //   }
+    // );
+
+    this._sharepointService.getMockData().subscribe(
+      (result: Timeline[]) => {
+        console.log(result);
+        this.data = result;
+        this.totalItems = result.length;
+      },
+      error => console.log(error)
+    );
+
+  }
 
   ngOnInit() {
   }
--- src/app/components/timeline/timeline.component.ts
+++ src/app/components/timeline/timeline.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { SharepointService } from '../../services/sharepoint.service';
 import { Timeline } from './timeline';
+
 
 @Component({
   selector: 'app-timeline',
@@ -11,49 +12,55 @@ export class TimelineComponent implements OnInit {
 
   itemSelect: Timeline;
   listaNoticias: Timeline[] = [];
-  constructor(private _sharepointService: SharepointService) {
+  currentPage = 1;
+  itemPorPage = 5;
 
-    // this._sharepointService.getItems().subscribe(
-    //   (result: any) => {
-    //     this.data = result.d.results;
-    //     console.log('This.data :' + this.data);
+  @Input() data: Timeline[];
+  @Input() totalItems: number;
 
-    //     for (let i = 0; i < this.data.length; i++) {
-    //       // tslint:disable-next-line:prefer-const
-    //       let item: TimelineElement = {
-    //         caption: '',
-    //         title: this.data[i].Title,
-    //         content: this.data[i].Descripcion,
-    //         visible: this.data[i].Visible,
-    //         date: new Date(this.data[i].Fecha),
-    //         link: this.data[i].Link.Url
-    //       };
-    //       this.listaNoticias.push(item);
-    //     }
-    //     console.log(this.listaNoticias);
-    //   },
-    //   error => {
-    //     console.log(<any>error);
-    //   }
-    // );
 
-    this._sharepointService.getMockData().subscribe(
-      (result: Timeline[]) => {
-        console.log(result);
-        this.listaNoticias = result;
-      },
-      error => console.log(error)
-    );
+
+  constructor() {
 
   }
 
   ngOnInit() {
   }
 
+  getTotalPages() {
+    return  Math.ceil(this.totalItems / this.itemPorPage);
+  }
 
-  clickSelect( e: Event, item: Timeline) {
+  getPages() {
+
+  }
+
+  isValidPageNumber(page: number, totalPages: number): boolean {
+    return page > 0 && page <= totalPages;
+  }
+
+
+  selectItem(e: Event, item: Timeline) {
     e.preventDefault();
     this.itemSelect = item;
   }
+
+  selectPage( e: Event) {
+    this.currentPage = this.currentPage + 1;
+    console.log(this.currentPage);
+    this.data = this.data.slice( (this.currentPage - 1 ) * this.itemPorPage, this.currentPage * this.itemPorPage);
+  }
+
+  onPrev() {
+    if (this.currentPage < 1) {
+        return;
+    }
+
+    this.currentPage = this.currentPage--;
+    console.log(this.currentPage);
+
+  }
+
+
 
 }
